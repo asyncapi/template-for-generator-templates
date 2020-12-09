4e2488fdebfbd85e0473ff90f5d8e3f5f5d1e48a
derberg <lpgornicki@gmail.com>
2020-12-09 18:10:47 +0100

    more debug

--- test/hooks/generateExtraFormats.test.js
+++ test/hooks/generateExtraFormats.test.js
@@ -4,20 +4,21 @@ const Generator = require('@asyncapi/generator');
 const dummySpecUrl = 'https://rawcdn.githack.com/asyncapi/generator/v1.0.1/test/docs/dummy.yml';
 //you always want to generate to new directory to make sure test runs in clear environment
 const outputDir = path.resolve('test/temp/generateExtraFormats', Math.random().toString(36).substring(7));
-const generator = new Generator(path.resolve(__dirname, '../../'), outputDir, { 
-  forceWrite: true,
-  templateParams: {
-    svg: 'true',
-    png: 'true',
-    pdf: 'true'
-  }
-});
 
 describe('generateExtraFormats()', () => {
   jest.setTimeout(30000);
 
-  beforeAll(async() => await generator.generateFromURL(dummySpecUrl));
-
+  beforeAll(async() => {
+    const generator = new Generator(path.resolve(__dirname, '../../'), outputDir, { 
+      forceWrite: true,
+      templateParams: {
+        svg: 'true',
+        png: 'true',
+        pdf: 'true'
+      }
+    });
+    await generator.generateFromURL(dummySpecUrl);
+  });
   it('svg diagram file is generated', async () => {
     const svg = existsSync(path.join(outputDir, 'index.svg'));
     expect(svg).toBeTruthy();
--- test/template/templateGenerationResult.test.js
+++ test/template/templateGenerationResult.test.js
@@ -1,12 +1,10 @@
 const { readFile } = require('fs').promises;
+const { existsSync } = require('fs');
 const path = require('path');
 const Generator = require('@asyncapi/generator');
 const dummySpecUrl = 'https://rawcdn.githack.com/asyncapi/generator/v1.0.1/test/docs/dummy.yml';
 //you always want to generate to new directory to make sure test runs in clear environment
 const outputDir = path.resolve('test/temp/templateGenerationResult', Math.random().toString(36).substring(7));
-const generator = new Generator(path.resolve(__dirname, '../../'), outputDir, { 
-  forceWrite: true
-});
 
 describe('templateGenerationResult()', () => {
   jest.setTimeout(30000);
@@ -14,10 +12,18 @@ describe('templateGenerationResult()', () => {
   console.log('template path', path.resolve(__dirname, '../../'));
   console.log('read path', path.join(outputDir, 'index.html'));
 
-  beforeAll(async() => await generator.generateFromURL(dummySpecUrl));
+  beforeAll(async() => {
+    const generator = new Generator(path.resolve(__dirname, '../../'), outputDir, { 
+      forceWrite: true
+    });
+    await generator.generateFromURL(dummySpecUrl);
+  });
 
   it('generated correct index.html with diagram source', async () => {
     console.log('read attempt');
+    const html = existsSync(path.join(outputDir, 'index.html'));
+    expect(html).toBeTruthy();
+
     const index = await readFile(path.join(outputDir, 'index.html'), 'utf8');
     console.log(index);
     expect(index).toMatchSnapshot();
