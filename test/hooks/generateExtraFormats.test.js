ec25f045d82571bd4b20a62209871b8a67370f02
derberg <lpgornicki@gmail.com>
2020-12-09 17:09:29 +0100

    test: better structure for test output

--- test/hooks/generateExtraFormats.test.js
+++ test/hooks/generateExtraFormats.test.js
@@ -3,7 +3,7 @@ const path = require('path');
 const Generator = require('@asyncapi/generator');
 const dummySpecUrl = 'https://rawcdn.githack.com/asyncapi/generator/v1.0.1/test/docs/dummy.yml';
 //you always want to generate to new directory to make sure test runs in clear environment
-const outputDir = path.resolve('test/temp', Math.random().toString(36).substring(7));
+const outputDir = path.resolve('test/temp/generateExtraFormats', Math.random().toString(36).substring(7));
 const generator = new Generator(path.resolve(__dirname, '../../'), outputDir, { 
   forceWrite: true,
   templateParams: {
--- test/template/templateGenerationResult.test.js
+++ test/template/templateGenerationResult.test.js
@@ -3,11 +3,11 @@ const path = require('path');
 const Generator = require('@asyncapi/generator');
 const dummySpecUrl = 'https://rawcdn.githack.com/asyncapi/generator/v1.0.1/test/docs/dummy.yml';
 //you always want to generate to new directory to make sure test runs in clear environment
-const outputDir = path.resolve('test/temp', Math.random().toString(36).substring(7));
+const outputDir = path.resolve('test/temp/generateExtraFormats', Math.random().toString(36).substring(7));
 const generator = new Generator(path.resolve(__dirname, '../../'), outputDir, { 
   forceWrite: true
 });
-describe('generateExtraFormats()', () => {
+describe('templateGenerationResult()', () => {
   jest.setTimeout(30000);
 
   it('generated correct index.html with diagram source', async () => {
