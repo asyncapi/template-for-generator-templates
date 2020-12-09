542820b467d62408b02dde74587b5a647f5d53a0
derberg <lpgornicki@gmail.com>
2020-12-09 12:10:18 +0100

    test: refactor to fix timeout

--- test/hooks/generateExtraFormats.test.js
+++ test/hooks/generateExtraFormats.test.js
@@ -2,30 +2,25 @@ const { readFile } = require('fs').promises;
 const path = require('path');
 const Generator = require('@asyncapi/generator');
 const dummySpecUrl = 'https://rawcdn.githack.com/asyncapi/generator/v1.0.1/test/docs/dummy.yml';
+//you always want to generate to new directory to make sure test runs in clear environment
+const outputDir = path.resolve('test/temp', Math.random().toString(36).substring(7));
+const generator = new Generator(path.resolve(__dirname, '../../'), outputDir, { 
+  forceWrite: true,
+  templateParams: {
+    svg: 'true',
+    png: 'true'
+  }
+});
 
 describe('generateExtraFormats()', () => {
-  //you always want to generate to new directory to make sure test runs in clear environment
-  const outputDir = path.resolve('test/temp', Math.random().toString(36).substring(7));
-
-  beforeAll(async () => {
-    console.log('before generation');
-    const generator = new Generator(path.resolve(__dirname, '../../'), outputDir, { 
-      forceWrite: true,
-      templateParams: {
-        svg: 'true',
-        png: 'true'
-      }
-    });
-    await generator.generateFromURL(dummySpecUrl);
-    console.log('after generation');
-  });
-
   it('generates correct svg diagram', async () => {
+    await generator.generateFromURL(dummySpecUrl);
     const svg = await readFile(path.join(outputDir, 'index.svg'), 'utf8');
     expect(svg).toMatchSnapshot();
   });
 
   it('generates correct png diagram', async () => {
+    await generator.generateFromURL(dummySpecUrl);
     const png = await readFile(path.join(outputDir, 'index.png'), 'utf8');
     expect(png).toMatchSnapshot();
   });
--- test/template/templateGenerationResult.test.js
+++ test/template/templateGenerationResult.test.js
@@ -2,19 +2,14 @@ const { readFile } = require('fs').promises;
 const path = require('path');
 const Generator = require('@asyncapi/generator');
 const dummySpecUrl = 'https://rawcdn.githack.com/asyncapi/generator/v1.0.1/test/docs/dummy.yml';
-
+//you always want to generate to new directory to make sure test runs in clear environment
+const outputDir = path.resolve('test/temp', Math.random().toString(36).substring(7));
+const generator = new Generator(path.resolve(__dirname, '../../'), outputDir, { 
+  forceWrite: true
+});
 describe('generateExtraFormats()', () => {
-  //you always want to generate to new directory to make sure test runs in clear environment
-  const outputDir = path.resolve('test/temp', Math.random().toString(36).substring(7));
-  const generator = new Generator(path.resolve(__dirname, '../../'), outputDir, { 
-    forceWrite: true
-  });
-
-  beforeAll(async () => {
-    await generator.generateFromURL(dummySpecUrl);
-  });
-
   it('generated correct index.html with diagram source', async () => {
+    await generator.generateFromURL(dummySpecUrl);
     const index = await readFile(path.join(outputDir, 'index.html'), 'utf8');
     expect(index).toMatchSnapshot();
   });
