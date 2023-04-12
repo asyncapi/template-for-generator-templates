85ac746b95f7652b8b648da7ebb3d252e4959611
derberg <lpgornicki@gmail.com>
2023-04-12 13:32:25 +0200

    adding some logs

--- test/template/templateGenerationResult.test.js
+++ test/template/templateGenerationResult.test.js
@@ -1,8 +1,5 @@
-/**
- * @jest-environment node
- */
-
 const { readFile } = require('fs').promises;
+const { existsSync } = require('fs');
 const path = require('path');
 const Generator = require('@asyncapi/generator');
 const dummySpecPath = path.join(path.resolve(__dirname, '../../'), 'test/fixtures/dummy.yml');
@@ -10,11 +7,15 @@ const dummySpecPath = path.join(path.resolve(__dirname, '../../'), 'test/fixture
 const outputDir = path.resolve('test/temp/templateGenerationResult', Math.random().toString(36).substring(7));
 
 describe('templateGenerationResult()', () => {
-  jest.setTimeout(10000000);
+  jest.setTimeout(100000);
 
   beforeAll(async() => {
+    console.log('output dir', outputDir)
+    console.log('template path', path.resolve(__dirname, '../../'))
     const generator = new Generator(path.resolve(__dirname, '../../'), outputDir, { forceWrite: true });
     await generator.generateFromFile(dummySpecPath);
+    console.log('index.html location', path.join(outputDir, 'index.html'))
+    console.log('index.html isexists', existsSync(path.join(outputDir, 'index.html')))
   });
 
   it('generated correct index.html with diagram source', async () => {
